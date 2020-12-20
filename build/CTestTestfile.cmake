ac001f5c30a092a3437ced2859ccc928bf378cdd
leizhao3 <zl.stone1992@gmail.com>
2020-12-19 19:07:04 -0800

    first comment

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,20 @@
+# CMake generated Testfile for 
+# Source directory: /home/odroid/catkin_ws/src
+# Build directory: /home/odroid/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("navigation_tutorials/navigation_stage")
+subdirs("navigation_tutorials/navigation_tutorials")
+subdirs("navigation_tutorials/roomba_stage")
+subdirs("navigation_tutorials/laser_scan_publisher_tutorial")
+subdirs("navigation_tutorials/point_cloud_publisher_tutorial")
+subdirs("rplidar_ros")
+subdirs("mpu6050_serial_to_imu")
+subdirs("navigation_tutorials/odometry_publisher_tutorial")
+subdirs("rf2o_laser_odometry")
+subdirs("navigation_tutorials/robot_setup_tf_tutorial")
+subdirs("navigation_tutorials/simple_navigation_goals_tutorial")
+subdirs("robot_localization")
+subdirs("hypha-racecar")
---
+++ devel/share/gennodejs/ros/robot_localization/srv/_index.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+let ToLL = require('./ToLL.js')
+let ToggleFilterProcessing = require('./ToggleFilterProcessing.js')
+let FromLL = require('./FromLL.js')
+let SetDatum = require('./SetDatum.js')
+let SetPose = require('./SetPose.js')
+let GetState = require('./GetState.js')
+
+module.exports = {
+  ToLL: ToLL,
+  ToggleFilterProcessing: ToggleFilterProcessing,
+  FromLL: FromLL,
+  SetDatum: SetDatum,
+  SetPose: SetPose,
+  GetState: GetState,
+};
